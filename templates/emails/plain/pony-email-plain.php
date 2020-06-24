08acacfaaf22f7e634bd377be16364e251dbbb9a
James Pham <jamespham93@yahoo.com>
2020-06-24 14:29:33 -0700

    Created Pony Email template

---
+++ templates/emails/plain/pony-email-plain.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Customer "Pony" order email
+ */
+$order = new WC_order( $item_data->order_id );
+
+echo "= " . $email_heading . " =\n\n";
+
+$opening_paragraph = __( 'An order, made by %s, has now been marked Pony. The details of the item are as follows:' );
+
+$customer = new WC_Customer( $order->get_customer_id() );
+$billing_first_name = $customer->get_first_name();
+$billing_last_name = $customer->get_last_name();
+
+if ( $order && $billing_first_name && $billing_last_name ) {
+    echo sprintf( $opening_paragraph, $billing_first_name . ' ' . $billing_last_name ) . "\n\n";
+}
+
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+
+echo sprintf( __( 'Ordered Product: %s', 'finishing-email' ), $item_data->product_title ) . "\n";
+
+echo sprintf( __( 'Quantity: %s', 'finishing-email' ), $item_data->qty ) . "\n";
+
+echo sprintf( __( 'Total: %s', 'finishing-email' ), $item_data->total ) . "\n";
+
+echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+
+echo __( 'This is an email sent as the order status has been changed to "Finishing".', 'pony-email' ) . "\n\n";
+
+echo apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) );
---
+++ templates/emails/pony-email-html.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Customer "Pony" order status email
+ */
+$order = new WC_order( $item_data->order_id );
+$opening_paragraph = __( 'An order, made by %s, has now been marked Pony. The details of the item are as follows:' );
+
+?>
+
+<?php do_action( 'woocommerce_email_header', $email_heading ); ?>
+
+<?php
+$customer = new WC_Customer( $order->get_customer_id() );
+$billing_first_name = $customer->get_first_name();
+$billing_last_name = $customer->get_last_name();
+
+if ( $order && $billing_first_name && $billing_last_name ) : ?>
+    <p><?php printf( $opening_paragraph, $billing_first_name . ' ' . $billing_last_name ); ?></p>
+<?php endif; ?>
+
+<?php
+/*
+* @hooked WC_Emails::order_details() Shows the order details table.
+* @hooked WC_Structured_Data::generate_order_data() Generates structured data.
+* @hooked WC_Structured_Data::output_structured_data() Outputs structured data.
+* @since 2.5.0
+*/
+do_action( 'woocommerce_email_order_details', $order );
+
+/*
+* @hooked WC_Emails::order_meta() Shows order meta data.
+*/
+do_action( 'woocommerce_email_order_meta', $order );
+
+/*
+* @hooked WC_Emails::customer_details() Shows customer details
+* @hooked WC_Emails::email_address() Shows email address
+*/
+do_action( 'woocommerce_email_customer_details', $order );
+
+/**
+ * Show user-defined additional content - this is set in each email's settings.
+ */
+if ( $additional_content ) {
+    echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+}
+
+do_action( 'woocommerce_email_footer' );
+?>
