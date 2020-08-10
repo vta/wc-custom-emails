26f3341293525ad1b1a89af4e5e3d8766f6ba629
James Pham <jamespham93@yahoo.com>
2020-08-10 14:24:02 -0700

    Changed form to anchor tag/link. Added styling

--- templates/emails/ready-reminder-email-html.php
+++ templates/emails/ready-reminder-email-html.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Customer email to reminder custom that order is Ready for Pick Up
+ * Customer email reminder that order is Ready for Pick Up
  */
 $order = new WC_order( $item_data->order_id );
 $opening_paragraph = __( '%s, this is a reminder email that your order is ready for pick up. Please come get your order 
@@ -17,12 +17,26 @@ $billing_last_name = $customer->get_last_name();
 if ( $order && $billing_first_name && $billing_last_name ) : ?>
     <p><?php printf( $opening_paragraph, $billing_first_name . ' ' . $billing_last_name ); ?></p>
     <p>
-        If you already picked up your order, click on the link below to complete your order. You may need to log in to do
-        the following action
-        <form action="<?php echo site_url() . '/my-account/view-order/' . $order->get_id() ?>" method="get">
-            <input id="completed" type="hidden" name="completed" value="1">
-            <button type="submit">I already picked up my order</button>
-        </form>
+        If you already picked up your order, click on the link below to complete your order. You may need to log in to complete
+        the following action.
+        <a href="<?php echo site_url() . '/my-account/view-order/' . $order->get_id() . '?completed=1' ?>"
+                style="background: #017aca;
+                        color: #fff;
+                        border: none;
+                        cursor: pointer;
+                        display: inline-block;
+                        font-weight: 600;
+                        letter-spacing: 0.0333em;
+                        line-height: 1.25;
+                        margin: 1rem 0;
+                        opacity: 1;
+                        padding: 1.1em 1.44em;
+                        text-align: center;
+                        text-decoration: none;
+                        text-transform: uppercase;
+                        transition: opacity 0.15s linear;">
+            I already picked up my order
+        </a>
     </p>
 <?php endif; ?>
 
